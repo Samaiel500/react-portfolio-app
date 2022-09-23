6449caaa754dc794251085bec2dfa78f25bd4783
Stanislav <samaiel500@gmail.com>
2022-09-23 18:06:23 +0300

    finish v1.0 footer compleate and emailjs

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contacts.css'
 import { MdOutlineMail, MdContactPhone } from 'react-icons/md'
-import {BsWhatsapp} from 'react-icons/bs'
+import { BsWhatsapp } from 'react-icons/bs'
+import emailjs from 'emailjs-com'
+
 
 const Contacts = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_orckigs', 'template_1yozt3e', form.current, 'PSRTraaNVOUf5WlP-')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    e.target.reset();
+  };
+
   return (
     <section id='contacts'>
       <h5>Get In Touch</h5>
@@ -31,7 +47,7 @@ const Contacts = () => {
           </article>
         </div>
 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required/>
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
